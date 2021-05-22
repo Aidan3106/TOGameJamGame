16f299bf89428690688ad7821eb40916116d5498
KyleGabiane <72885112+KyleGabiane@users.noreply.github.com>
2021-05-21 21:48:53 -0400

    Adding Respawn Script for Level 2

---
+++ Assets/Respawn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+  
+     [SerializeField] private Transform player;
+     [SerializeField] private Transform RespawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.transform.position = RespawnPoint.transform.position;
+    }
+
+}
